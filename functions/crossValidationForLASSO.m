0d349d067f00c9ba2e066f699847eb1d71b99108
Ahasier <fangyu@wustl.edu>
2023-09-19 17:02:34 -0500

    debugged

--- functions/crossValidationForLASSO.m
+++ functions/crossValidationForLASSO.m
@@ -1,25 +1,20 @@
-function [optimalCoefficients, optimalLambda, bestOutSampleError] = crossValidationForLASSO(testData, testOutput, coefficients, maxLambda, threshold)
+function [optimalCoefficients, optimalLambda, bestOutSampleError] = crossValidationForLASSO(testData, testOutput, coefficients, maxLambda)
 % initialize variables
 Lambda = setLambdaRange(maxLambda);
 len1 = length(Lambda);
 allOutSampleErrors = zeros(len1, 1);
 allR2OutSample = zeros(len1, 1);
 allCoefficients = zeros(size(coefficients, 1), len1);
-weights = setWeightsRange();
 
 % Loop through all regresssion (lambda) parameters
 for l = 1:len1
     % Get estimated coefficients at the current regresssion (lambda) parameters
     currentCoefficients = coefficients(:, l);
-    
-    % Compute out-of-sample mean squared error and r^2 on the test data to pick an
-    % optimal one with minimal MSE
-    [optimallyWeightedCoefficients, optimallyWeightedOutSampleError] = pickOptimalWeightForThresholding(weights, threshold, currentCoefficients, testData, testOutput);
-    
+        
     % Store results
-    allR2OutSample(l) = computeRSquared(testData, testOutput, optimallyWeightedCoefficients);
-    allOutSampleErrors(l) = optimallyWeightedOutSampleError;
-    allCoefficients(:, l) = optimallyWeightedCoefficients;
+    allR2OutSample(l) = computeRSquared(testData, testOutput, currentCoefficients);
+    allOutSampleErrors(l) = computeSquaredError(testData, testOutput, currentCoefficients);
+    allCoefficients(:, l) = currentCoefficients;
 end
 [bestOutSampleError, idx] = min(allOutSampleErrors(:));
 optimalCoefficients = allCoefficients(:, idx);
